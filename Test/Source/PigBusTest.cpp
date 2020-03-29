510c595783f3ce64a181a2ec70453cb5c551865f
Andreas Hansen <andrhan@gmail.com>
2020-03-29 16:05:26 +0200

    initial

---
+++ Test/Source/PigBusTest.cpp
@@ -0,0 +1,14 @@
+#include "gtest.h"
+#include "gmock.h"
+#include <memory>
+
+using ::testing::Return;
+using ::testing::An;
+using ::testing::Eq;
+using ::testing::Matcher;
+using ::testing::TypedEq;
+
+TEST(PigBusTest, Oink)
+{
+
+}
